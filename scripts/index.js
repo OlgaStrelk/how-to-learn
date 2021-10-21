ade71bf492aff7740025fab156e302d21cc6b809
Strelkova Olga <strelod@yandex.ru>
2021-10-21 14:43:27 +0300

    feat: debag funct

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const popupAddPost = document.querySelector(".popup");
 const buttonPopupOpen = document.querySelector(".advices__button");
 const containerPosts = document.querySelector(".advices");
+const templatePost = document.getElementById('post').content;
 
 const togglePopup = (popupName) => {
   popupName.classList.toggle("popup_is-opened");
@@ -9,3 +10,8 @@ const togglePopup = (popupName) => {
 buttonPopupOpen.addEventListener("click", () => {
   togglePopup(popupAddPost)
 });
+
+const createPost = (data) => {
+  const postElement = templatePost.querySelector(".post__item").cloneNode(true);
+
+}
